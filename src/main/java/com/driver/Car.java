29ceef8ab42cba4277ab019d99647d688bbc1d8a
Danish Suhail <134198476+Danish-Suhail@users.noreply.github.com>
2026-05-14 02:06:18 +0530

    Refactor changeGear and changeSpeed methods

--- src/main/java/com/driver/Car.java
+++ src/main/java/com/driver/Car.java
@@ -23,11 +23,15 @@ public class Car extends Vehicle {
 
     public void changeGear(int newGear){
         this.currentGear = newGear;
-        System.out.println("changeGear method called - The gear is changed to: " + currentGear);
+        System.out.println("changeGear method called - The gear is changed to: " + this.currentGear);
     }
 
     public void changeSpeed(int newSpeed, int newDirection){
-        move(newSpeed, newDirection);
+        super.move(newSpeed, newDirection);
         System.out.println("changeSpeed method called - The speed is changed to: " + newSpeed + ", and the direction is changed to: " + newDirection + " degrees");
+    }
+
+    public int getCurrentGear(){
+        return this.currentGear;
     }
 }
